61954f68aa1d9e15f700cf84a2a639c820e0005b
Danil Yarantsev <tiberiumk12@gmail.com>
2021-03-15 20:25:06 +0300

    update the zig cc script

--- zig-compile-vps.sh
+++ zig-compile-vps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 nim c -d:release --stackTrace:on --lineTrace:on \
 --cc:clang --clang.exe="zigcc" --clang.linkerexe="zigcc" \
---passC:"-target x86_64-linux-gnu.2.24 -O2" --passL:"-target x86_64-linux-gnu.2.24 -O2" -o:ircordvps src/ircord.nim
+--passC:"-target x86_64-linux-gnu.2.24 -fno-sanitize=undefined" \
+--passL:"-target x86_64-linux-gnu.2.24 -fno-sanitize=undefined" -o:ircordvps src/ircord.nim
