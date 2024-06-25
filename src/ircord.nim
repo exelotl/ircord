1857410fb611c127ae7ca1f0cf7be2b7a77dab07
Jeremy Clarke <gecko@exelo.tl>
2024-06-25 16:34:48 +0100

    don't crash if magic gbadev channels don't exist

--- src/ircord.nim
+++ src/ircord.nim
@@ -485,9 +485,9 @@ proc messageCreate(s: Shard, m: Message) {.async.} =
 
   let name = get(m.member?.nick, m.author.username)
   let toSend =
-    if m.channel_id == discordIdByName["gbajam-news"]:
+    if m.channel_id == discordIdByName.getOrDefault("gbajam-news"):
       &"\x02[New Announcement]\x02 {msg}"
-    elif m.channel_id == discordIdByName["showcase"]:
+    elif m.channel_id == discordIdByName.getOrDefault("showcase"):
       &"\x02Showcase post from {name}:\x02 {msg}"
     else:
       # Use bold styling to highlight the username
